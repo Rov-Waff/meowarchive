6510b975651a4e5356f37bec9dc5643c3c783f4e
xiaole233 <2710182206@qq.com>
2026-08-19 22:37:18 +0800

    更改changelog

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,8 +6,9 @@ export default async function Home() {
       <div className="border rounded-lg p-2 border-gray-300 shadow-md">
         <p>本站备份了毛毡2016-2024的所有可以被找到的的帖子，您可以进行检索</p>
         <p>站长:xiaole6324 xiaole602010@qq.com</p>
-        <p>感谢编程追梦者为我提供了一点精神支持（雾）</p>
+        <p>感谢编程追梦者为我提供了这个站的favicon</p>
         <p>v1.1.0:基于PostgreSQL，做了一个简单的搜索功能，不一定好用<br />目前接口速度慢到极致，我实在没什么好办法（（</p>
+	<p>v1.1.1:更换favicon</p>
       </div>
       <form
         action="/search"
