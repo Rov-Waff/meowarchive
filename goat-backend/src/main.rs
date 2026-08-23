3dbffeec45bb5619eee1482c28d2201af90de479
xiaole6324 <2710182206@qq.com>
2026-08-23 15:02:43 +0800

    feat: 生成实体类，搭建后端服务

--- goat-backend/src/main.rs
+++ goat-backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok();
+    let app = Router::new().route("/", get(||async{"Fuck you"}));
+    let listener = TcpListener::bind(format!(
+        "0.0.0.0:{}",
+        env::var("SERVER_PORT").expect("No port provided")
+    ))
+    .await
+    .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
