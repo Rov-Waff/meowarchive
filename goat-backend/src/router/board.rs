7986c408a2462cdbfceb9eef12e08e4e62e5f678
xiaole6324 <2710182206@qq.com>
2026-08-24 10:51:39 +0800

    feat: 获取板块信息API

--- goat-backend/src/router/board.rs
+++ goat-backend/src/router/board.rs
@@ -1,11 +1,15 @@
 use std::sync::Arc;
 
-use axum::{Json, Router, extract::State, http::StatusCode, routing::get};
+use axum::{
+    Json, Router,
+    extract::{Path, State},
+    http::StatusCode,
+    routing::get,
+};
 use sea_orm::{DbErr, EntityTrait};
 
 use crate::{AppState, entity};
 
-#[axum::debug_handler]
 async fn get_all_board_handler(
     state: State<Arc<AppState>>,
 ) -> Result<Json<Vec<entity::board::Model>>, (StatusCode, String)> {
@@ -17,6 +21,23 @@ async fn get_all_board_handler(
     Ok(Json(res))
 }
 
+async fn get_board_info(
+    board_id: Path<i64>,
+    state: State<Arc<AppState>>,
+) -> Result<Json<entity::board::Model>, (StatusCode, String)> {
+    let db = state.db.clone();
+    match entity::board::Entity::find_by_id(*board_id)
+        .one(&db)
+        .await
+        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?
+    {
+        Some(res) => Ok(Json(res)),
+        None => Err((StatusCode::NOT_FOUND, "Not Found".to_string())),
+    }
+}
+
 pub fn board_router() -> Router<Arc<AppState>> {
-    Router::new().route("/", get(get_all_board_handler))
+    Router::new()
+        .route("/", get(get_all_board_handler))
+        .route("/{board_id}", get(get_board_info))
 }
