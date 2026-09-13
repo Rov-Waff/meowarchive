860f377a9e54d3bd0a40f90f09ea6c191e48b7b2
xiaole6324 <2710182206@qq.com>
2026-09-13 11:32:03 +0800

    feat:删除旧的Python后端

--- backend/app/dtos/__init__.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class PageResult[T](BaseModel):
-    total_page: int
-    current_page: int
-    has_prev: bool
-    has_next: bool
-    item: list[T]
--- backend/main.py
+++
@@ -1,7 +0,0 @@
-import dotenv
-import uvicorn
-
-if __name__ == "__main__":
-    # Develop Environment
-    dotenv.load_dotenv()
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
